00f70cc90860f3946e29aa5cb2caf7ca85e7b683
Cole Sellers <csellers18@yahoo.com>
2018-09-19 10:07:19 -0400

    Update IPFinder2.java

--- IPFinder2.java
+++ IPFinder2.java
@@ -1,22 +1,19 @@
-import java.net.*; // for InetAddress
+import java.net.*; 
 
 public class IPFinder2 {
 	public static void main(String[] args) {
 		// Get name and IP address of the local host
 		try {
-			InetAddress address = InetAddress.getLocalHost(); // Gets the local host’s IP address in object format
+			InetAddress address = InetAddress.getLocalHost(); 
 			System.out.println("Local Host:");
-			System.out.println("\t" + address.getHostName()); //
+			System.out.println("\t" + address.getHostName()); 
 		} catch (UnknownHostException e) {
 			System.out.println("Unable to determine this host's address");
 		}
 		for (int i = 0; i < args.length; i++) {
-		// Get name(s)/address(es) of hosts given on command-line
 			try {
-				// Create an array of InetAddress instances for the specified host
 				InetAddress[] addressList = InetAddress.getAllByName(args[i]);
 				System.out.println(args[i] + ":");
-				// Print the first name and all associated IP addresses. Assume array contains at least one entry.
 				System.out.println("\t" + addressList[0].getHostName());
 			for (int j = 0; j < addressList.length; j++)
 				System.out.println("\t" + addressList[j].getHostAddress());
